93f81f03255b64d17ed8f59bd8caa7e6c1ec76f6
Winston Smith <wsbraves@gmail.com>
2019-09-10 22:03:17 -0400

    create initial functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+async function getWords(input) {
+  const response = await fetch(`https://api.datamuse.com/words?sl=${input}`);
+  const json = await response.json();
+  return json;
+}
+
+class App extends React.Component {
+  state = { words: [], userInput: "" };
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" id="userInput" onChange={this.handleChange} />
+          <button type="submit">Find</button>
+        </form>
+        {this.state.words.map(w => (
+          <Word key={w.word} word={w.word} />
+        ))}
+      </div>
+    );
+  }
+  async componentDidMount() {
+    this.setState({ words: await getWords("ever dance") });
+  }
+  componentDidUpdate() {
+    console.log(this.state);
+  }
+  handleSubmit = async e => {
+    e.preventDefault();
+    this.setState({ words: await getWords(this.state.userInput) });
+  };
+  handleChange = e => {
+    this.setState({ userInput: e.target.value });
+  };
+}
+
+function Word(props) {
+  return <p>{props.word}</p>;
 }
 
 export default App;
