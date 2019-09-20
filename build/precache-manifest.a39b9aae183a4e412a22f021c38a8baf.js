88c8f48675aad36a0a68d93ab070d7a6b934671c
Winston Smith <wsbraves@gmail.com>
2019-09-20 15:35:27 -0400

    build commit

---
+++ build/precache-manifest.a39b9aae183a4e412a22f021c38a8baf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d92f42cc217ed9cba6c7d6dd8b7c8091",
+    "url": "/malapro-gen/index.html"
+  },
+  {
+    "revision": "55ee04d5e18c7e08a314",
+    "url": "/malapro-gen/static/css/main.2db19b8a.chunk.css"
+  },
+  {
+    "revision": "62902925c89528e55a71",
+    "url": "/malapro-gen/static/js/2.57d7195d.chunk.js"
+  },
+  {
+    "revision": "55ee04d5e18c7e08a314",
+    "url": "/malapro-gen/static/js/main.5d8f97d8.chunk.js"
+  },
+  {
+    "revision": "44bd4e276642874a6b9d",
+    "url": "/malapro-gen/static/js/runtime~main.8a307717.js"
+  }
+]);
